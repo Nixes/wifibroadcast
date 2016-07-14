4347c0b58fbc5ddaa0ce232a0e74915d90026e06
Nixes <callum@see-bry.com>
2016-07-14 14:58:33 +1000

    bash is dumb

--- scripts/settings.sh
+++ scripts/settings.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# note, spacing in the variable declaration syntax is important, follow to the letter
+
 # multiple interfaces may be listed here to enable diversity
 rx_radio_interface="wlx7cdd907ebe01"
 
@@ -10,7 +12,7 @@ tx_radio_interface="wlan0"
 radio_channel="14"
 
 # these are the command line arguments to be used for ./tx and ./rx
-fec_settings = "-b 8 -r 16 -f 1024"
+fec_settings="-b 8 -r 16 -f 1024"
 
 # mjpeg is more bandwidth heavy, and so fec settings should be set lower
-mjpeg_fec_settings = "-b 8 -r 4 -f 1024"
+mjpeg_fec_settings="-b 8 -r 4 -f 1024"
