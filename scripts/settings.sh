e274b1411ff7df3768ca5be527ed3883bdc25846
Nixes <callum@see-bry.com>
2016-07-14 14:37:20 +1000

    update settings syntax

--- scripts/settings.sh
+++ scripts/settings.sh
@@ -2,10 +2,10 @@
 rx_radio_interface="wlx7cdd907ebe01"
 
 # only one interface may be listed here
-tx_radio_interface=wlan0
+tx_radio_interface="wlan0"
 
 # this makes sure settings are the same between tx and rx
-radio_channel=14
+radio_channel="14"
 
 # these are the command line arguments to be used for ./tx and ./rx
 fec_settings = "-b 8 -r 16 -f 1024"
