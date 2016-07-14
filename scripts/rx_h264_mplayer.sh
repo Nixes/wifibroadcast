64363f961985bf21ba2b8d13be69b100031612e0
Nixes <callum@see-bry.com>
2016-07-14 15:40:21 +1000

    add some missed settings vars

--- scripts/rx_h264_mplayer.sh
+++ scripts/rx_h264_mplayer.sh
@@ -3,4 +3,4 @@
 # this dot space notation works like an include
 . ./rx_prep_radio.sh
 
-./rx -b 8 -r 16 -f 1024 $rx_radio_interface | mplayer -fps 200 -demuxer h264es -nocache -
+./rx $fec_settings $rx_radio_interface | mplayer -fps 200 -demuxer h264es -nocache -
--- scripts/rx_mjpeg_mplayer.sh
+++ scripts/rx_mjpeg_mplayer.sh
@@ -3,4 +3,4 @@
 # this dot space notation works like an include
 . ./rx_prep_radio.sh
 
-./rx -b 8 -r 4 -f 1024 $rx_radio_interface | mplayer -fps 200 -demuxer lavf -nocache -
+./rx $mjpeg_fec_settings $rx_radio_interface | mplayer -fps 200 -demuxer lavf -nocache -
